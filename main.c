81b0a7bfd5b70b2247f93277c14a074573f37950
Szymon Pomieciński <szumon612@interia.pl>
2022-12-01 13:34:37 +0100

    ukonczono zadanie pierwsze

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+void pierwsze(){
+    int liczba1 = 67;
+    float liczba2 = ((float)liczba1);
+    printf("Liczba z dokladnoscia do jednego miejsca po przecinku wynosi: %.1f\n", liczba2);
+    printf("Liczba z dokladnoscia do trzech miejsc po przecinku wynosi: %.3f\n", liczba2);
+    printf("Liczba w formacie napisowym to: %c\n", liczba1);
+}
 
 int main() {
-    printf("Hello, World!\n");
+
     return 0;
 }

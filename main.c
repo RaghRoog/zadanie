904b63c9abb18716bd4af81ea462c22651b1daf8
Szymon Pomieciński <szumon612@interia.pl>
2022-12-01 13:36:44 +0100

    ukonczono zadanie trzecie

--- main.c
+++ main.c
@@ -17,7 +17,18 @@ void drugie(){
     printf("Liczba %d to %c w ASCII\n", kod, kod);
 }
 
-
+void trzecie(){
+    float wzrost;
+    float waga;
+    float BMI;
+    printf("Ten program oblicza BMI\n");
+    printf("Podaj swoj wzrost [m]\n");
+    scanf("%f", &wzrost);
+    printf("Podaj swoja wage [kg]\n");
+    scanf("%f", &waga);
+    BMI = waga / (wzrost * wzrost);
+    printf("Twoje BMI wynosi: %.2f\n", BMI);
+}
 
 int main() {
 
